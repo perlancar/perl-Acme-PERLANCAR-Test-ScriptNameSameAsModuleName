11aee52583cef4dd1a05c69573d3496a3dcfb0f7
perlancar (@pc-office) <perlancar@gmail.com>
2016-02-10 19:15:45 +0700

    Initial commit

---
+++ lib/Acme/PERLANCAR/Test/ScriptNameSameAsModuleName.pm
@@ -0,0 +1,16 @@
+package Acme::PERLANCAR::Test::ScriptNameSameAsModuleName;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Test script name that is the same as a known module name
+
+=head1 DESCRIPTION
+
+This distribution contains the following scripts:
+
+# INSERT_EXECS_LIST
+
+which have the same name as some known module names. This distribution is for
+testing purposes only.
